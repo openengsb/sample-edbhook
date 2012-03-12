1d5e9465dd9d42dce1c6e2b601b1e8477c0c42dd
Christoph Hochreiner <ch.hochreiner@gmail.com>
2012-03-12 11:19:14 +0100

    added updatehook

---
+++ updateHook/src/main/java/org/openengsb/framework/edb/updateHook/internal/UpdateHook.java
@@ -0,0 +1,17 @@
+package org.openengsb.framework.edb.updateHook.internal;
+
+import java.util.List;
+
+import org.openengsb.core.api.edb.EDBObject;
+import org.openengsb.core.api.edb.EDBUpdateHook;
+import org.openengsb.similarity.Indexer;
+
+public class UpdateHook implements EDBUpdateHook {
+
+    private Indexer indexer;
+
+    @Override
+    public void update(List<EDBObject> inserts, List<EDBObject> updates, List<EDBObject> deletes) {
+        indexer.updateIndex(inserts, updates, deletes);
+    }
+}
