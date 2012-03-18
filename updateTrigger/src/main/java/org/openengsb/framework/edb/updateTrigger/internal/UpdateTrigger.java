f46e49de988db9dbebdc7cfc8b6307464b0240b7
Christoph Hochreiner <ch.hochreiner@gmail.com>
2012-03-18 21:07:40 +0100

    added alternative architecture

--- updateTrigger/src/main/java/org/openengsb/framework/edb/updateTrigger/internal/UpdateTrigger.java
+++ updateTrigger/src/main/java/org/openengsb/framework/edb/updateTrigger/internal/UpdateTrigger.java
@@ -1,20 +1,26 @@
 package org.openengsb.framework.edb.updateTrigger.internal;
 
+import java.io.IOException;
+
 import org.openengsb.core.api.edb.EDBCommit;
 import org.openengsb.core.api.edb.EDBUpdateTrigger;
 import org.openengsb.similarity.Index;
 
 public class UpdateTrigger implements EDBUpdateTrigger {
 
-    private Index index;
+    private Index index = null;
+
+    public UpdateTrigger() {
+        try {
+            index = new StandardIndex();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void update(EDBCommit commit) {
         index.updateIndex(commit);
-    }
-
-    public void setIndex(Index index) {
-        this.index = index;
     }
 
 }
